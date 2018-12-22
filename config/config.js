d13afefc4e8125a73fcf0b9fe790b357a74671e9
Temitayo Oyelowo <temitayooyelowo@gmail.com>
2018-12-22 11:36:10 -0500

    Updated configuration data

--- config/config.js
+++ config/config.js
@@ -1,8 +1,15 @@
+if(!!process.env.NODE_ENV){
+    return;
+}
+process.env.NODE_ENV = 'development';
 const configJSON = require("./config.json");
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const configJSONObjects = configJSON[process.env.NODE_ENV];
 
 for(let key in configJSONObjects) {
     process.env[key] = configJSONObjects[key];
 }
+
+
+
+
